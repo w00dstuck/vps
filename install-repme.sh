3f3695a5af480acada0c15dfa518baa1696970c4
w00dstuck <49167699+w00dstuck@users.noreply.github.com>
2019-04-01 21:13:03 +0200

    Create install-repme.sh

---
+++ install-repme.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+cd ~
+cd /usr/local/bin
+wget https://www.dropbox.com/s/amuijm16wgahbll/RepMe-.Daemon_Ubuntu_16.04.gz
+tar -zxvf RepMe-.Daemon_Ubuntu_16.04.gz
+rm -rf RepMe-.Daemon_Ubuntu_16.04.gz
+sleep 30
+ls
