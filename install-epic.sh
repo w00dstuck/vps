798925b0eda19d611f0416e54a95527f8a1104d4
w00dstuck <49167699+w00dstuck@users.noreply.github.com>
2019-04-24 23:01:36 +0200

    Create install-epic.sh

---
+++ install-epic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~
+cd /usr/local/bin
+wget https://github.com/EpicCrypto/Epic/releases/download/2.2.1/epic-2.2.1-x86_64-linux-gnu.tar.gz
+tar -zxvf epic-2.2.1-x86_64-linux-gnu.tar.gz
+rm -rf epic-2.2.1-x86_64-linux-gnu.tar.gz
