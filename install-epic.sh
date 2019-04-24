44d380fa7bfbc9b1b9d7f702b123949fbc2d08f3
w00dstuck <49167699+w00dstuck@users.noreply.github.com>
2019-04-24 23:19:47 +0200

    Update install-epic.sh

--- install-epic.sh
+++ install-epic.sh
@@ -5,3 +5,12 @@ cd /usr/local/bin
 wget https://github.com/EpicCrypto/Epic/releases/download/2.2.1/epic-2.2.1-x86_64-linux-gnu.tar.gz
 tar -zxvf epic-2.2.1-x86_64-linux-gnu.tar.gz
 rm -rf epic-2.2.1-x86_64-linux-gnu.tar.gz
+
+echo "Installing dependencies..."
+apt-get -qq update
+apt-get -qq upgrade
+apt-get -qq autoremove
+apt-get -qq install wget htop unzip
+apt-get -qq install build-essential && apt-get -qq install libtool autotools-dev autoconf libevent-pthreads-2.0-5 automake && apt-get -qq install libssl-dev && apt-get -qq install libboost-all-dev && apt-get -qq install software-properties-common && add-apt-repository -y ppa:bitcoin/bitcoin && apt update && apt-get -qq install libdb4.8-dev && apt-get -qq install libdb4.8++-dev && apt-get -qq install libminiupnpc-dev && apt-get -qq install libqt4-dev libprotobuf-dev protobuf-compiler && apt-get -qq install libqrencode-dev && apt-get -qq install git && apt-get -qq install pkg-config && apt-get -qq install libzmq3-dev
+apt-get -qq install aptitude
+apt-get -qq install libevent-dev
