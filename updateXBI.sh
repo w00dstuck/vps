f1b589130b2b376d589008a26e094e0275b78c6e
w00dstuck <49167699+w00dstuck@users.noreply.github.com>
2020-06-10 22:52:29 +0200

    Update updateXBI.sh

--- updateXBI.sh
+++ updateXBI.sh
@@ -23,7 +23,7 @@ MAG='\e[1;35m'
 
 function purgeOldInstallation() {
     echo -e "${GREEN}Preparing the VPS to setup to update $COIN_NAME masternode${NC}"
-    echo -e "${GREEN}* Searching and removing old $COIN_NAME files and configurations${NC}"
+    echo -e "${GREEN}* Removing old $COIN_NAME cli and deamon${NC}"
 #kill wallet daemon
     systemctl stop $COIN_NAME.service > /dev/null 2>&1
     killall $COIN_DAEMON > /dev/null 2>&1
@@ -33,6 +33,56 @@ function purgeOldInstallation() {
     rm -rf /usr/local/bin/$COIN_CLI /usr/local/bin/$COIN_DAEMON> /dev/null 2>&1
     rm -rf /usr/bin/$COIN_CLI /usr/bin/$COIN_DAEMON > /dev/null 2>&1
     echo -e "    ${YELLOW}> Done${NC}";
+}
+
+function prepare_system() {
+
+echo -e "${GREEN}* Making sure system is up to date...${NC}"
+apt-get update > /dev/null 2>&1
+apt-get -y upgrade > /dev/null 2>&1
+
+
+
+export DEBIAN_FRONTEND=noninteractive
+aptget_params='--quiet -y'
+
+echo -e "    ${GREEN}> Adding bitcoin PPA repository"
+apt-add-repository -y ppa:bitcoin/bitcoin > /dev/null 2>&1
+apt-get ${aptget_params} install software-properties-common > /dev/null 2>&1
+
+dpkg --clear-avail > /dev/null 2>&1
+apt-get ${aptget_params} update > /dev/null 2>&1
+apt-get --quiet -f install > /dev/null 2>&1
+dpkg --configure -a > /dev/null 2>&1
+
+# intentional duplicate to avoid some errors
+apt-get ${aptget_params} update > /dev/null 2>&1
+apt-get ${aptget_params} upgrade > /dev/null 2>&1
+
+echo -e "    ${GREEN}> Installing required packages, it may take some time to finish.${NC}"
+package_list="build-essential libtool curl autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 ufw libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-dev libevent-1.4-2 libdb4.8-dev libdb4.8++-dev autoconf libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev libminiupnpc-dev git multitail vim unzip unrar htop ntpdate"
+apt-get ${aptget_params} install ${package_list} > /dev/null 2>&1 || apt-get ${aptget_params} install ${package_list} > /dev/null 2>&1
+
+apt-get ${aptget_params} install ${package_list} > /dev/null 2>&1 || apt-get ${aptget_params} install ${package_list} > /dev/null 2>&1
+
+apt-get ${aptget_params} update > /dev/null 2>&1
+apt-get ${aptget_params} upgrade > /dev/null 2>&1
+if [ "$?" -gt "0" ];
+  then
+    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
+    echo "apt-get update"
+    echo "apt -y install software-properties-common"
+    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
+    echo "apt-get update"
+    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ unzip libzmq5"
+ exit 1
+fi
+echo -e "    ${GREEN}> synchronize time${NC}"; sleep 0.5s
+ntpdate -s time.nist.gov
+
+clear
 }
 
 function download_node() {
@@ -89,6 +139,7 @@ function restart_service() {
 clear
 
 purgeOldInstallation
+prepare_system
 download_node
 add_bootstrap
 restart_service
